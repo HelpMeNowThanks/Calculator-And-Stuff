a0a301f9a01e2c213cbba7f4134f5efb0ca7abc4
HelpMeNowThanks <67766329+HelpMeNowThanks@users.noreply.github.com>
2021-06-26 01:33:58 +0200

    Update converter.py

--- converter.py
+++ converter.py
@@ -9,21 +9,25 @@ def MIL_to_KM():
     num4 = input("Enter Miles: ")
     result4 = float(km_in_miles) * float(num4)
     print(result4)
+    a=input("Press enter to exit: ")
 def KM_to_MIL():
     miles_in_km = 0.621371
     num3 = input("Enter KMs: ")
     result3 = float(miles_in_km) * float(num3)
     print(result3)
+    b=input("Press enter to exit: ")
 def M_to_F():
     feet_in_meter = 3.28084
     num1 = input("Enter Meters: ")
     result1 = float(feet_in_meter) * float(num1)
     print(result1)
+    c=input("Press enter to exit: ")
 def F_to_M():
     meter_in_feet = 0.3048
     num2 = input("Enter feet: ")
     result2 = float(meter_in_feet) * float(num2)
     print(result2)
+    d=input("Press enter to exit: ")
 if choice == "1":
     M_to_F()
 elif choice == "2":
